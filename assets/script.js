34c0e6844edb63c1a2f3ade78659715ac4b2fe33
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-02-02 19:13:00 -0500

    added UV and 5 days API

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,35 @@
 var apiKey = "6193f46f0e9c03ae7f8a936482891767";
-var searchBtnEl = document.getElementById("search-btn")
-var searchInputEl = document.getElementById("search-input")
+var searchBtnEl = document.getElementById("search-btn");
+var searchInputEl = document.getElementById("search-input");
 
-searchBtnEl.addEventListener("click", clickMe)
+searchBtnEl.addEventListener("click", clickMe);
 function clickMe() {
-    getApi(searchInputEl.value)
+  getApi(searchInputEl.value);
 }
 
-
 function getApi(city) {
-  // TODO: Insert the API url to get a list of your repos
   var currentWthrUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=` +
-  apiKey;
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=` + apiKey;
 
   fetch(currentWthrUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-     console.log(data)
+      console.log(data);
+      getOneCall(data.coord.lat, data.coord.lon);
     });
 }
 
+function getOneCall(lat, lon) {
+  var currentWthrUrl =
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=` + apiKey;
+
+  fetch(currentWthrUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
